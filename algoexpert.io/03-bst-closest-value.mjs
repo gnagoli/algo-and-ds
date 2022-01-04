09ccb0662f0dde9f0a3c0e18a80bc9d1a7870cc9
Luckmann <lkgnagoli@gmail.com>
2022-01-04 11:08:37 +0100

    solve closest value in bst problem

---
+++ algoexpert.io/03-bst-closest-value.mjs
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import BST from "./23-bst-construction.mjs"; // for test purpose
+
+//  Iterative method
+// Average : O(log(n)) time | O(1) space
+// Worst : O(n) time | O(1) space
+function closestValueInBst(bst, value) {
+  let closestValue = Infinity;
+  let currentNode = bst;
+  while (currentNode) {
+    if (Math.abs(currentNode.value - value) < Math.abs(closestValue - value)) {
+      closestValue = currentNode.value;
+    }
+    if (currentNode.value < value) {
+      currentNode = currentNode.rigth;
+    } else if (currentNode.value > value) {
+      currentNode = currentNode.left;
+    } else break;
+  }
+  return closestValue;
+}
+
+//  recursive method
+// Average : O(log(n)) time | O(log(n)) space
+// Worst : O(n) time | O(n) space
+function closestValueInBst2(bst, value) {
+  return (function closestValueHelper(currentNode, targetValue, closest) {
+    if (!currentNode) return closest;
+    if (Math.abs(currentNode.value - value) < Math.abs(closest - value)) {
+      closest = currentNode.value;
+    }
+    if (currentNode.value < value) {
+      return closestValueHelper(currentNode.rigth, targetValue, closest);
+    } else if (currentNode.value > value) {
+      return closestValueHelper(currentNode.left, targetValue, closest);
+    } else return closest;
+  })(bst, value, Infinity);
+}
+
+const tree = new BST(20);
+tree.insert(8);
+tree.insert(12).insert(132);
+
+console.log(closestValueInBst(tree, 10));
+console.log(closestValueInBst2(tree, 10));
